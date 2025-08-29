c5b59f3e495e5ca40d7b23a61386d87bc209e08f
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-29 16:04:43 +0600

    calling-features

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let count = 0;
 
+
 // Fav Count
 const favBtn = document.getElementsByClassName('fav-btn');
 for (let i = 0; i < favBtn.length; i++) { 
@@ -53,4 +54,66 @@ const copyBtns = document.getElementsByClassName('copy-btn');
 // Call Button
 
 const callBtn = document.getElementsByClassName('call-btn');
-alert('')
+const coins = document.getElementById('coin-count');
+for (let i = 0; i < callBtn.length; i++) {
+  callBtn[i].addEventListener('click', function() {
+    const currentCoins = parseInt(coins.innerText);
+    if (currentCoins >= 20) {
+      coins.innerText = currentCoins - 20;
+    }
+
+    else {
+      alert('Insufficient Balance')
+    }
+  })
+}
+
+// Call Button Pop-Up
+
+document.getElementById('emergency-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling... National Emergency Number 999...')
+})
+
+document.getElementById('police-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling... Police Helpline Number 999...')
+})
+
+document.getElementById('fire-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling... Fire Service Number 999...')
+})
+
+document.getElementById('ambulance-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling... Ambulance Number 1994-999999...')
+})
+
+document.getElementById('women-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling... Women & Child Helpline Number 109...')
+})
+
+document.getElementById('corruption-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling... Anti-Corruption Helpline Number 106...')
+})
+
+document.getElementById('electricity-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling... Electricity Helpline Number 16216...')
+})
+
+
+document.getElementById('brac-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling...Brac Helpline Number 16445...')
+})
+
+
+document.getElementById('travel-btn')
+.addEventListener('click', function() {
+  alert ('📞 Calling... Bangladesh Railway Helpline Number 163...')
+})
+
